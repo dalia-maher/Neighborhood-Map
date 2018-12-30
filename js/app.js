c33a0e434882431a751f8ec1a3a7b88525a2071a
dalia-maher <dalia.maher94@gmail.com>
2018-12-30 23:40:28 +0200

    Adding location to the model

--- js/app.js
+++ js/app.js
@@ -5,9 +5,11 @@ var neighborhood = function(data) {
     var self = this;
 
     self.name = data.name;
+    self.location = data.location;
     self.info = data.info;
 
     self.marker = new google.maps.Marker({
         map: map,
+        position: self.location
     });
 };
