08bae4262e776389361b48acb9818b600c771fa2
dalia-maher <dalia.maher94@gmail.com>
2018-12-31 23:08:48 +0200

    Adding initMap function to application JS

--- js/app.js
+++ js/app.js
@@ -13,3 +13,16 @@ var neighborhood = function(data) {
         position: self.location
     });
 };
+
+var map;
+
+// Create a new blank array for all the listing markers.
+var markers = [];
+
+function initMap() {
+	// Constructor creates a new map - only center and zoom are required.
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 40.7413549, lng: -73.9980244},
+		zoom: 13
+	});
+}
